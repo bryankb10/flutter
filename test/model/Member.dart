a3850dbdc1e4244e1ff2e76eda0d6082592006fb
bryankb10 <bk.bryankenneth@gmail.com>
2025-03-25 22:04:43 +0800

    Create Member.dart

---
+++ test/model/Member.dart
@@ -0,0 +1,13 @@
+class Member {
+  String nik, nama, gender, status, agama, kerja;
+  int gaji;
+  Member(
+    this.nik,
+    this.nama,
+    this.gender,
+    this.status,
+    this.agama,
+    this.kerja,
+    this.gaji,
+  );
+}
